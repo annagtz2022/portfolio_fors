16543fdbb7c877fa4b6eaac194fcb55408407a02
Anna Goetz <anna.gtz087@outlook.com>
2023-09-11 21:46:48 +0200

    some changes

--- script.js
+++ script.js
@@ -36,14 +36,14 @@ $(document).ready(function(){
   // typing text animation script
   var typed = new Typed(".typing", {
       //strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],//
-      strings: ["Interpreter"],
+      strings: ["Linguist"],
       typeSpeed: 100,
       backSpeed: 60,
       loop: true
   });
 
   var typed = new Typed(".typing-2", {
-      strings: ["Interpreter"],
+      strings: ["Linguist"],
       typeSpeed: 100,
       backSpeed: 60,
       loop: true
